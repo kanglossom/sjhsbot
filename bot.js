8f4e29ec0acb1b70d7fa85bde0bf9e55ba3a1c3c
Kyle Choi <89368351+kylechoi0404@users.noreply.github.com>
2022-09-02 08:05:31 +0900

    commmit 7

--- bot.js
+++ bot.js
@@ -1,70 +1,68 @@
-const scriptname = "sjhsBot";
-/**
- * (string) room
- * (string) sender
- * (boolean) isGroupChat
- * (void) replier.reply(message)
- * (boolean) replier.reply(room, message, hideErrorToast = false) // 전송 성공시 true, 실패시 false 반환
- * (string) imageDB.getProfileBase64()
- * (string) packageName
- */
-function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
-  if(msg.startWith('/도움말')) {
-      replier.reply(help());
-  }
-  if(msg.startWith('/인사')) {
-    replier.reply(sayhilist());
-  }
-  hiReply();
-}
-
-var hiMsg = ['ㄱㅅ', 'ㅅㄱ'];
-var repliMsg = [
-  '고맙다~',
-  '웅 너나 수고해~'
-];
-
-function hiReply(){
-  let msgRepl = msg;
-  if(hiMsg.indexOf(msg) != -1){
-    replier.reply(repliMsg[himsg.indexOf(himsg)]);
-  }else{
-    break;
-  }
-}
-
-
-
-//아래 4개의 메소드는 액티비티 화면을 수정할때 사용됩니다.
-function onCreate(savedInstanceState, activity) {
-  var textView = new android.widget.TextView(activity);
-  textView.setText("Hello, World!");
-  textView.setTextColor(android.graphics.Color.DKGRAY);
-  activity.setContentView(textView);
-}
-
-function onStart(activity) {}
-
-function onResume(activity) {}
-
-function onPause(activity) {}
-
-function onStop(activity) {}
-
-function help(){
-  let msg = '안녕하세요! 선정봇이예요 >.0';
-  const help_msg = [
-    '/도움말 : 도움말을 보여줘요.',
-    '/인사 : 선정봇과 인사해요.'
-  ];
-  msg += help_msg.join('\n');
-
-  return msg;
-}
-
-
-function sayhilist() {
-  let msg = '다음의 단어에 선정봇이 작동합니다.'
-  const hi_msg = ['ㄱㅅ', 'ㅅㄱ']
-}
-
+const scriptname = "sjhsBot"; 
+ /** 
+  * (string) room 
+  * (string) sender 
+  * (boolean) isGroupChat 
+  * (void) replier.reply(message) 
+  * (boolean) replier.reply(room, message, hideErrorToast = false) // 전송 성공시 true, 실패시 false 반환 
+  * (string) imageDB.getProfileBase64() 
+  * (string) packageName 
+  */ 
+ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) { 
+   if(msg.startsWith('!도움말')) { 
+       replier.reply(help( )); 
+   } 
+   if(msg.startsWith('!인사')) { 
+     replier.reply(sayhilist()); 
+   } 
+   hiReply(); 
+ } 
+  
+ var hiMsg = ['ㄱㅅ', 'ㅅㄱ']; 
+ var repliMsg = [ 
+   '고맙다~', 
+   '웅 너나 수고해~' 
+ ]; 
+  
+ function hiReply(){ 
+   let msgRepl = msg; 
+   if(hiMsg.indexOf(msg) != -1){ 
+     replier.reply(repliMsg[himsg.indexOf(himsg)]); 
+   }
+  
+  
+  
+ //아래 4개의 메소드는 액티비티 화면을 수정할때 사용됩니다. 
+ function onCreate(savedInstanceState, activity) { 
+   var textView = new android.widget.TextView(activity); 
+   textView.setText("Hello, World!"); 
+   textView.setTextColor(android.graphics.Color.DKGRAY); 
+   activity.setContentView(textView); 
+ } 
+  
+ function onStart(activity) {} 
+  
+ function onResume(activity) {} 
+  
+ function onPause(activity) {} 
+  
+ function onStop(activity) {} 
+  
+ function help( ){ 
+   let msg = '안녕하세요! 선정봇이예요 >.0\n';
+   const help_msg = [ 
+     '!도움말 : 도움말을 보여줘요.', 
+     '!인사 : 선정봇과 인사해요.' 
+   ]; 
+   msg += help_msg.join('\n'); 
+  
+   return msg; 
+ } 
+  
+  
+ function sayhilist() { 
+   let msg = '다음의 단어에 선정봇이 작동합니다.' 
+   const hi_msg = ['ㄱㅅ', 'ㅅㄱ'] ;
+ }
+ 
+ }  
