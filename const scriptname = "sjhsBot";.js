b2c1b4553f9c69d46c44d9e4590156553cc1fedf
Kyle Choi <89368351+kylechoi0404@users.noreply.github.com>
2022-09-01 10:42:13 +0900

    bot

---
+++ const scriptname = "sjhsBot";.js
@@ -0,0 +1,33 @@
+const scriptname = "sjhsBot";
+/**
+ * (string) room
+ * (string) sender
+ * (boolean) isGroupChat
+ * (void) replier.reply(message)
+ * (boolean) replier.reply(room, message, hideErrorToast = false) // 전송 성공시 true, 실패시 false 반환
+ * (string) imageDB.getProfileBase64()
+ * (string) packageName
+ */
+function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
+  if(msg.startWith('/도움말')) {
+      replier.reply(help());
+  }
+}
+
+//아래 4개의 메소드는 액티비티 화면을 수정할때 사용됩니다.
+function onCreate(savedInstanceState, activity) {
+  var textView = new android.widget.TextView(activity);
+  textView.setText("Hello, World!");
+  textView.setTextColor(android.graphics.Color.DKGRAY);
+  activity.setContentView(textView);
+}
+
+function onStart(activity) {}
+
+function onResume(activity) {}
+
+function onPause(activity) {}
+
+function onStop(activity) {}
+
+
