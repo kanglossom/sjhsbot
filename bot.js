1aab7032aef57c9750a8bdfede1ecccb8551bbe8
Kyle Choi <89368351+kylechoi0404@users.noreply.github.com>
2022-09-11 23:14:44 +0900

    rere

--- bot.js
+++ bot.js
@@ -89,8 +89,8 @@ function bobnymnym() {
         //급식정보 가져와서 표기해야함.
     var now = new Date();
     var mon = now.getMonth();
-    var year = now.getFullYear();
-    var year=string.substring(2,3)
+    var year1 = now.getFullYear();
+    var year=str.substring(2,3);
     var day = now.getDay();
     mon += 1;
     
@@ -99,7 +99,7 @@ function bobnymnym() {
     var ymd = year+mon+day;
     
     var url = "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=6b15c10192db4d8194e4b3c1b5df01c5&Type=json&plndex=1&pSize=30&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010191&MLSV_YMD="+ymd;
-    var result = Utils.getWebText(url,false,false)
+    var result = Utils.getWebText(url,false,false);
     
     try{
                     
@@ -133,3 +133,4 @@ function bobnymnym() {
 
     return msg;
 }
+
