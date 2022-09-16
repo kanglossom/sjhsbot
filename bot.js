b86ec900b55a028fe50f33e3bc751b8201659583
Kyle Choi <89368351+kylechoi0404@users.noreply.github.com>
2022-09-16 14:59:07 +0900

    try-change

--- bot.js
+++ bot.js
@@ -15,31 +15,18 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
     if (msg.startsWith('!도움말')) {
         replier.reply(help());
     }
-    if (msg.startsWith('!인사')) {
-        replier.reply(sayhilist());
-    }
     if (msg.startsWith('!한강물온도')) {
+        replier.reply("한강물뜨러 다녀올게요!");
         replier.reply(Hwater());
     }
 
     if (msg.startsWith('!급식')) {
+        replier.reply("오늘 급식이 뭐드라...");
         replier.reply(bobnymnym());
     }
 
-    function hiReply() {
-        if (hiMsg.indexOf(msg) != -1) {
-            let msg = repliMsg[hiMsg.indexOf(hiMsg)];
-        }
-        return msg;
-    }
-    hiReply();
+    
 }
-
-var hiMsg = ['ㄱㅅ', 'ㅅㄱ'];
-var repliMsg = [
-    '고맙다~',
-    '웅 너나 수고해~'
-];
 
 
 //아래 4개의 메소드는 액티비티 화면을 수정할때 사용됩니다. 
@@ -62,7 +49,6 @@ function help() {
     let msg = '안녕하세요! 선정봇이예요 >.0\n 아래 명령어를 입력하시면 선정봇이 작동해요! \n';
     const help_msg = [
         '!도움말 : 도움말을 보여줘요.',
-        '!인사 : 선정봇과 인사해요.',
         '!한강물온도 : 한강물 온도를 알려줘요.',
         '!급식 : 급식정보를 알려줘요.'
     ];
@@ -71,33 +57,8 @@ function help() {
     return msg;
 }
 
-
-function sayhilist() {
-    let msg = '다음의 단어에 선정봇이 작동합니다.'
-    const hi_msg = ['ㄱㅅ', 'ㅅㄱ'];
-
-    msg += hi_msg.join('\n');
-    return msg;
-}
-
-function Hwater() {
-
-  //let hangang_server_raw = org.jsoup.Jsoup.connect("https://api.hangang.msub.kr/").get().text();
-   //let hangang_json = JSON.parse(hangang_server_raw);
-   //var msg0 = "현재 한강 수온은 " + hangang_json.temp + "도 입니다.";
-   //{"station":"노량진","status":"success","temp":"22.1","time":"24:00","type":"hangangAPI"}
-   //status 사용해서 연결 실패시도 해보기
-   let Hwater0 = fetch("https://api.hangang.msub.kr/")
-   .then((response) => response.json())
-   .then((data) => data);
-    //fetch is not defined
-
-    var msg0 = "현재 한강 수온은 " + data.temp + "도 입니다.";
-    return msg0;
-}
-
 function bobnymnym() {
-    let msg = '배고프냐?ㅋㅋ'
+    let msg = '';
         //참고할 페이지는 웨일에 스크랩해둠.
         //7010191 - 학교코드 (B10)
         //코드의 순서(?) 먼저 매일매일의 날짜를 가져와서 n월 n일 급식입니다. 시전하기.
@@ -105,20 +66,30 @@ function bobnymnym() {
     var now = new Date();
     var mon = now.getMonth();
     var year = now.getFullYear();
-    var year=string.substring(2,3)
-    var day = now.getDay();
+    var day = now.getDate();
+    var yoil = now.getDay();
     mon += 1;
+    mon = String( mon );
+    day = String( day );
+    if(mon.length==1){
+        mon = "0" + mon;
+    }
+    if(day.length==1){
+        day = "0" + day;
+    }
     
-    //var yoil = ["일", "월", "화", "수", "목", "금", "토"];
-    let dateMsg = mon + "월 " + day + "일 급식입니다!" ;
+    
+    var yoilHan = ["월","화","수","목","금","토","일"];
+    let dateMsg = mon + "월 " + day + "일 " +yoilHan[yoil] + "요일 급식이래! \n";
     var ymd = year+mon+day;
     
-    var url = "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=6b15c10192db4d8194e4b3c1b5df01c5&Type=json&plndex=1&pSize=30&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010191&MLSV_YMD="+ymd;
-    var result = Utils.getWebText(url,false,false)
+    let url = "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=6b15c10192db4d8194e4b3c1b5df01c5&Type=json&plndex=1&pSize=30&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010191&MLSV_YMD="+ymd;
+    let result = Utils.getWebText(url,false,false).split("<body>")[1].split("</body>")[0];
     
     try{
                     
-        calories = result.split("CAL_INFO\":\"")[1].split("\",\"NTR_INFO")[0];
+        //calories = result.split("CAL_INFO\":\"")[1].split("\",\"NTR_INFO")[0];
+        result = result.split("중식")[1];
         result = result.split("\",\"ORPLC")[0].split("\"DDISH_NM\":\"")[1].replace(/(<([^>]+)>)/g, "");
         result = result.replace(/amp;/gi, "");
         result = result.replace(/undefined/gi,"");
@@ -132,20 +103,38 @@ function bobnymnym() {
         
         
 
-    }
-    catch(e){
-        replier.reply("급식 정보가 없습니다");
+    }catch(e){
+        msg += "급식 정보가 없다는데?";
+        
         
     }
-    result += "\n";
-            result += "총 ";
-            result += calories;
-   
+    
     msg += dateMsg;
     msg += result;
-
-
 
     return msg;
 }
 
+function Hwater() {
+
+     let url = ""; //서울시 열린데이터 광장에서 갖고오면 될듯 :)
+     let result = Utills.getWebText(url,false,false).split("<body>")[1].split("</body>")[0];
+
+     try{
+                    
+        //여따 파싱하기
+        
+        
+
+    }catch(e){
+        msg += "저런. 한강가는길이 좀 막히네요.";
+        
+        
+    }
+
+     var msg0 = "지금 한강 수온은 " + result + "도래.";
+     //{"station":"노량진","status":"success","temp":"22.1","time":"24:00","type":"hangangAPI"}
+     //status 사용해서 연결 실패시도 해보기
+     
+      return msg0;
+  }
