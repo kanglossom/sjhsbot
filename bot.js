8167ba3a3565935a52059e01a942203b282bfa4c
Kyle Choi <89368351+kylechoi0404@users.noreply.github.com>
2022-09-16 13:54:41 +0900

    Hwater

--- bot.js
+++ bot.js
@@ -15,14 +15,13 @@ function response(room, msg, sender, isGroupChat, replier, imageDB, packageName)
     if (msg.startsWith('!도움말')) {
         replier.reply(help());
     }
-    if (msg.startsWith('!인사')) {
-        replier.reply(sayhilist());
-    }
     if (msg.startsWith('!한강물온도')) {
+        replier.reply("한강물뜨러 다녀올게요!");
         replier.reply(Hwater());
     }
 
     if (msg.startsWith('!급식')) {
+        replier.reply("오늘 급식이 뭐드라...");
         replier.reply(bobnymnym());
     }
 
@@ -62,7 +61,6 @@ function help() {
     let msg = '안녕하세요! 선정봇이예요 >.0\n 아래 명령어를 입력하시면 선정봇이 작동해요! \n';
     const help_msg = [
         '!도움말 : 도움말을 보여줘요.',
-        '!인사 : 선정봇과 인사해요.',
         '!한강물온도 : 한강물 온도를 알려줘요.',
         '!급식 : 급식정보를 알려줘요.'
     ];
@@ -71,18 +69,8 @@ function help() {
     return msg;
 }
 
-
-function sayhilist() {
-    let msg = '다음의 단어에 선정봇이 작동합니다.'
-    const hi_msg = ['ㄱㅅ', 'ㅅㄱ'];
-
-    msg += hi_msg.join('\n');
-    return msg;
-}
-
-
 function bobnymnym() {
-    let msg = '배고프냐?ㅋㅋ \n'
+    let msg = '';
         //참고할 페이지는 웨일에 스크랩해둠.
         //7010191 - 학교코드 (B10)
         //코드의 순서(?) 먼저 매일매일의 날짜를 가져와서 n월 n일 급식입니다. 시전하기.
@@ -104,11 +92,11 @@ function bobnymnym() {
     
     
     var yoilHan = ["월","화","수","목","금","토","일"];
-    let dateMsg = mon + "월 " + day + "일 " +yoilHan[yoil] + "요일 급식입니다! \n";
+    let dateMsg = mon + "월 " + day + "일 " +yoilHan[yoil] + "요일 급식이래! \n";
     var ymd = year+mon+day;
     
-    var url = "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=6b15c10192db4d8194e4b3c1b5df01c5&Type=json&plndex=1&pSize=30&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010191&MLSV_YMD="+ymd;
-    var result = Utils.getWebText(url,false,false).split("<body>")[1].split("</body>")[0];
+    let url = "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=6b15c10192db4d8194e4b3c1b5df01c5&Type=json&plndex=1&pSize=30&ATPT_OFCDC_SC_CODE=B10&SD_SCHUL_CODE=7010191&MLSV_YMD="+ymd;
+    let result = Utils.getWebText(url,false,false).split("<body>")[1].split("</body>")[0];
     
     try{
                     
@@ -128,7 +116,7 @@ function bobnymnym() {
         
 
     }catch(e){
-        msg += "급식 정보가 없습니다";
+        msg += "급식 정보가 없다는데?";
         
         
     }
@@ -143,3 +131,26 @@ function bobnymnym() {
     return msg;
 }
 
+function Hwater() {
+
+     let url = ""; //서울시 열린데이터 광장에서 갖고오면 될듯 :)
+     let result = Utills.getWebText(url,false,false).split("<body>")[1].split("</body>")[0];
+
+     try{
+                    
+        //여따 파싱하기
+        
+        
+
+    }catch(e){
+        msg += "저런. 한강가는길이 좀 막히네요.";
+        
+        
+    }
+
+     var msg0 = "지금 한강 수온은 " + result + "도래.";
+     //{"station":"노량진","status":"success","temp":"22.1","time":"24:00","type":"hangangAPI"}
+     //status 사용해서 연결 실패시도 해보기
+     
+      return msg0;
+  }
